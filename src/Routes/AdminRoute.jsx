5fead91d4543b4016243a3a8dc3af7d0c8b4a328
mahi8026 <mahimrahman07@gmail.com>
2025-12-14 01:09:13 +0600

    adding admin route

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import useRole from "../hooks/useRole"; // You created this hook in Part 3
+import React from "react";
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const { role, isLoading: isRoleLoading } = useRole();
+  const location = useLocation();
+
+  if (loading || isRoleLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
+
+  if (user && role === "admin") {
+    return children;
+  }
+
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
